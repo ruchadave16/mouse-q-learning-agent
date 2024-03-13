ea2cab1c904b81c3c0a6207d68a44c85fd75a995
Jessica Brown <91238623+jes-bro@users.noreply.github.com>
2024-03-12 21:48:13 -0400

    Fix bugs in matrix indexing and qTable dimensions

--- QLearning.kt
+++ QLearning.kt
@@ -1,12 +1,12 @@
 import kotlin.random.Random
 
 class QLearning(actionSpace: Int, private var worldDim: Int, defaultValue: Int, defaultState: String, private var alpha: Int) {
-    private val qTable: MutableList<MutableList<Int>> = MutableList(worldDim*2) { MutableList(actionSpace) { defaultValue } }
+    private val qTable: MutableList<MutableList<Int>> = MutableList(worldDim*worldDim) { MutableList(actionSpace) { defaultValue } }
     private val actions = setOf("U","D","L","R")
     private var gamma = 0.5
     private val world = MutableList(worldDim) { MutableList(worldDim) { defaultState } }
     private val rewards = mutableMapOf("Cheese" to 2, "Cookie" to 100, "Salad" to -2, "No food" to 0)
-    data class WorldState(var x: Int, var y: Int, var type: String)
+    data class WorldState(var row: Int, var col: Int, var type: String)
     private var agentState = WorldState(0, 0, world[0][0])
 
 
@@ -17,68 +17,75 @@ class QLearning(actionSpace: Int, private var worldDim: Int, defaultValue: Int, 
     fun getQTable(){
         for (row in qTable) {
             for (value in row) {
-                print("$value")
+                print("$value ")
             }
+            println()
         }
     }
 
     fun updateAgentState(action: String) {
         when (action) {
-            "U" -> agentState.y++
-            "D" -> agentState.y--
-            "L"-> agentState.x--
-            "R" -> agentState.x++
+            "U" -> agentState.row--
+            "D" -> agentState.row++
+            "L"-> agentState.col--
+            "R" -> agentState.col++
         }
     }
 
-    fun updateQTable(row:Int, col:Int, qValue: Int) {
-        qTable[row - 1][col - 1] = qValue
+    fun updateQTable(state: WorldState, action: String, qValue: Int) {
+        val row = (state.row*worldDim + state.col)
+        var col = 0
+        when (action) {
+            "U" -> col = 0
+            "D" -> col = 1
+            "L" -> col = 2
+            "R" -> col = 3
+        }
+        qTable[row][col] = qValue
     }
 
     fun qLookup(state: WorldState, action: String): Int {
         var col: Int = 0
         when (action) {
             "D" -> col = 1
-            "L"-> col = 2
+            "L" -> col = 2
             "R" -> col = 3
         }
-        val row = (state.x + worldDim + state.y) - 1
+        val row = (state.row*worldDim + state.col)
         return qTable[row][col]
     }
 
-    fun valueFunction(state: String): Int {
-        val rewardAtState = rewards[state]
-        //what is the next state? 
-        val v: Int = 0
-        return v
-    }
-
     fun computeReward(state: WorldState, action: String): Int {
+        val stateCopy = WorldState(state.row, state.col, state.type)
         when (action) {
-            "U" -> state.y++
-            "D" -> state.y--
-            "L" -> state.x--
-            "R" -> state.x++
+            "U" -> stateCopy.row--
+            "D" -> stateCopy.row++
+            "L" -> stateCopy.col--
+            "R" -> stateCopy.col++
         }
-        val nextStateType: String = world[state.x][state.y]
+        val nextStateType: String = world[state.row][state.col]
         return rewards[nextStateType]!!
     }
 
     // For q learning
     //Calculates q value, updates table
     fun qFunction(action: String) {
+        println("Start state: ${agentState.row}, ${agentState.col}")
         val currentQ = qLookup(agentState, action)
         val qMaximum = qMax()
         val currReward = computeReward(agentState, action)
+        println("Moving agent $action from ${agentState.row}, ${agentState.col}")
         updateAgentState(action)
         val newQ = currentQ + alpha * (currReward + gamma * qMaximum - currentQ)
-        updateQTable(agentState.x, agentState.y, newQ.toInt())
+        updateQTable(agentState, action, newQ.toInt())
     }
 
     fun qMax(): Int {
         val qList = mutableListOf<Int>()
         for (action in actions) {
-            qList.add(qLookup(agentState, action))
+            if (!isInvalidAction(agentState, action)) {
+                qList.add(qLookup(agentState, action))
+            }
         }
         return qList.max()
     }
@@ -89,13 +96,27 @@ class QLearning(actionSpace: Int, private var worldDim: Int, defaultValue: Int, 
         var maxQVal = -10000
         var qVal = -10000
         for (action in actions) {
-            qVal = qLookup(agentState, action)
-            if (maxQVal < qVal) {
-                maxQVal = qVal
-                maxAction = action
+            if (!isInvalidAction(agentState, action)) {
+                qVal = qLookup(agentState, action)
+                if (maxQVal < qVal) {
+                    maxQVal = qVal
+                    maxAction = action
+                }
             }
         }
         return maxAction
+    }
+
+    fun isInvalidAction(state: WorldState, action: String): Boolean {
+        val stateCopy = WorldState(state.row, state.col, state.type)
+        when (action) {
+            "U" -> stateCopy.row--
+            "D" -> stateCopy.row++
+            "L" -> stateCopy.col--
+            "R" -> stateCopy.col++
+        }
+
+        return (stateCopy.row > 3 || stateCopy.row < 0 || stateCopy.col > 3 || stateCopy.col < 0)
     }
 
     // For policy iteration
@@ -105,16 +126,24 @@ class QLearning(actionSpace: Int, private var worldDim: Int, defaultValue: Int, 
         val min = 0
         val max = 100
         // Chose an initial action at random
-        val firstAction = actions.random()
         var action = actions.random()
+        var yetToBeValid = true
+        while (yetToBeValid) {
+            action = actions.random()
+            yetToBeValid = isInvalidAction(agentState, action)
+        }
         // Update the q value based on the first action
-        qFunction(firstAction)
+        qFunction(action)
         // Then, update the policy (implicitly stored in the q function) for n steps using
         // an e-greedy approach (probability e that the action is random versus from the q table
         for (step in 0 until nSteps) {
             var num = Random.nextInt(0, 100)
+            yetToBeValid = true
             if (num < epsilon) {
-                action = actions.random()
+                while (yetToBeValid) {
+                    action = actions.random()
+                    yetToBeValid = isInvalidAction(agentState, action)
+                }
                 qFunction(action)
             } else {
                 action = qMaxAction()
@@ -127,11 +156,10 @@ class QLearning(actionSpace: Int, private var worldDim: Int, defaultValue: Int, 
 
 fun main() {
     val mouseAgent = QLearning(4, 4, 0, "No food",1)
-    mouseAgent.updateState(0, 0, "Cookie")
     mouseAgent.updateState(0, 1, "Salad")
     mouseAgent.updateState(2, 1, "Cheese")
-    mouseAgent.updateState(2, 2, "Cheese")
-    mouseAgent.trainQPolicy(100, 50)
+    mouseAgent.updateState(2, 2, "Cookie")
+    mouseAgent.trainQPolicy(1000, 50)
     println(mouseAgent.getQTable())
 
 }
